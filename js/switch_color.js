10d96f5de51c17378aaa1f1c5586baef646a9db9
levab <lev.it.up0@gmail.com>
2024-04-17 23:18:27 +0500

    Добавил 2ой файл JS для изменения стилей

---
+++ js/switch_color.js
@@ -0,0 +1,7 @@
+const footer1 = document.getElementById('foot');
+const word1 = document.getElementById('spn')
+
+footer1.style.color = "lime";
+footer1.style.fontSize = '18px';
+
+word1.style.fontSize = '30px'; 
